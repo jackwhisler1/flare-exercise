81262f8ab40655412b61a815f58efb5fea7a552c
jack <whisler1@gmail.com>
2022-01-25 16:30:12 -0600

    adds method to retreive api data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,25 @@
 <template>
   <h2>Table</h2>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      flights: [],
+    };
+  },
+  created: function () {
+    this.getFlightData();
+  },
+  methods: {
+    getFlightData: function () {
+      axios.get("https://flare-code-exercise-data.s3.amazonaws.com/airlines.json").then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
+};
+</script>
