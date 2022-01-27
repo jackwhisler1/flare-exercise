e2031a2818d9003eda92644d612e9175b853e0ad
jack <whisler1@gmail.com>
2022-01-26 18:14:48 -0600

    removes unused variables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-md p-5">
     <h2 class="p-2">Flight Data</h2>
-    <table v-if="flightData.length" class="table table-hover">
+    <table v-if="totalFlightData.length" class="table table-hover">
       <thead>
         <tr>
           <th scope="col" v-for="month in months" v-bind:key="month.id">{{ month }}</th>
@@ -25,11 +25,9 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      flightData: [],
+      totalFlightData: [],
       annualData: [],
-      airportCodes: [],
       months: [" ", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-      requestedStatistics: [],
       sortedByAirport: [],
       chunkedArray: [],
     };
@@ -42,36 +40,29 @@ export default {
     getFlightData: function () {
       axios.get("https://flare-code-exercise-data.s3.amazonaws.com/airlines.json").then((response) => {
         console.log(response.data);
-        this.flightData = response.data;
-        response.data.forEach(() => {
-          // placeholer
-        });
-        // 29 different airports
-        for (let i = 0; i < 29; i++) {
-          this.airportCodes.push(response.data[i].Airport.Code);
-        }
+        this.totalFlightData = response.data;
+        // Hardcoded for testing
         this.setYear(2008);
+        // Hardcoded for testing
         this.setFlightStatusAttribute("Statistics.Flights.Cancelled");
       });
-      console.log(this.airportCodes);
     },
     calculatePercentage: function (number, total) {
       return ((100 * number) / total).toFixed(1);
     },
     setYear: function (year) {
-      this.annualData = this.flightData.filter((flight) => flight.Time.Year === year);
+      this.annualData = this.totalFlightData.filter((flight) => flight.Time.Year === year);
       // organize array by airport
       this.sortedByAirport = this.annualData.sort((a, b) => {
         return a.Airport.Code.localeCompare(b.Airport.Code);
       });
       this.sortedByAirport = this.chunkArray(this.sortedByAirport, 12);
-      console.log(this.sortedByAirport);
-      console.log(this.annualData);
-      console.log("setting year");
+      // console.log(this.sortedByAirport);
+      // console.log(this.annualData);
+      // console.log("setting year");
     },
     setFlightStatusAttribute: function () {
-      // this.statistics = this.calculatePercent(this.annualData[attribute], this.annualData.Statistics.Total);
-      this.requestedStatistics = this.annualData.Statistics.Flights.Cancelled;
+      // placeholder for user dropdown selection
     },
     chunkArray: function (arr, val) {
       for (var i = 0; i < arr.length; i += val) {
